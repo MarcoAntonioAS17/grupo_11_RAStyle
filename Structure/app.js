5dc16ac516419292e44aaedda59f4e329f2b0b68
Brenda-RARE <brendacarmenrare@hotmail.com>
2021-06-14 00:52:28 -0500

    Login y Carrito de Compras

--- Structure/app.js
+++ Structure/app.js
@@ -7,10 +7,10 @@ const publicPath= path.resolve(__dirname, './public');
 app.use(express.static(publicPath));
 
 
-app.listen(3000, () => {
-    console.log('Servidor corriendo en el puerto 3000');
+app.listen(3005, () => {
+    console.log('Servidor corriendo en el puerto 3005');
 });
 
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './views/index.html'));
+    res.sendFile(path.resolve(__dirname, './views/login.html'));
 })
