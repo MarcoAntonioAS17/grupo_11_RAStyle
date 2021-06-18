a101ce33a1cf26ca8454e1f9e56bd25c0221e721
MarcoAntonioAS17 <antonioarcosmarco@gmail.com>
2021-06-18 15:23:43 -0500

    Puerto de servidor

--- Structure/app.js
+++ Structure/app.js
@@ -3,12 +3,14 @@ const path = require('path');
 
 const app = express();
 
+const PORT = process.env.PORT || 3000;
+
 const publicPath= path.resolve(__dirname, './public');
 app.use(express.static(publicPath));
 
 
-app.listen(3005, () => {
-    console.log('Servidor corriendo en el puerto 3005');
+app.listen(PORT, () => {
+    console.log('Servidor corriendo en el puerto ',PORT);
 });
 
 app.get('/', (req, res) => {
