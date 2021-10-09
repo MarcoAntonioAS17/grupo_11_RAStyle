a87da2f9cdcb477fb2959165b1a8beb336530e8e
MarcoAntonioAS17 <antonioarcosmarco@gmail.com>
2021-10-08 23:34:35 -0500

    Deploy database 2

--- Structure/src/database/config/config.js
+++ Structure/src/database/config/config.js
@@ -6,18 +6,18 @@ module.exports = {
     "host": "us-cdbr-east-04.cleardb.com",
     "dialect": "mysql"
   },
-  /* "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
+  "test": {
+    "username": "bb9ecc6cb7e41c",
+    "password": "484a7154",
+    "database": "heroku_87a32655bdfacd7",
+    "host": "us-cdbr-east-04.cleardb.com",
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
+    "username": "bb9ecc6cb7e41c",
+    "password": "484a7154",
+    "database": "heroku_87a32655bdfacd7",
+    "host": "us-cdbr-east-04.cleardb.com",
     "dialect": "mysql"
-  } */
+  }
 }
